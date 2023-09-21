048d123728a8c7063870e7efe05ced7b856af8f2
Mel-TB <melinda.trami@live.fr>
2023-09-21 19:11:10 +0200

    delete file

--- backend/postgres/seed/seed.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION;
-
-INSERT into users (name, email, entries, joined) VALUES ('Ezio', 'user@gmail.com', 6, '2023-09-12');
-INSERT into login (hash, email) VALUES ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'user@gmail.com');
-
-
-
-COMMIT;
